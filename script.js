c77019b9d8494d22e2e10c144b65106372325259
Mariusz Perzynski <m.perzyk@interia.pl>
2020-08-19 16:55:48 +0200

    add mode to move our square

--- script.js
+++ script.js
@@ -47,3 +47,37 @@ const createElement1 = function(){
 }
  bet.addEventListener("click", createElement1)
  
+
+
+
+ //skrolowanie okna z powiekszajacym i pomnijeszacym sie kwadratem #square
+
+
+ const changeSquare = document.querySelector('#square');
+ document.body.appendChild(square);
+
+let grow = true; //!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ let size = 100;
+ square.style.width = size + "px";
+ square.style.height = size + "px";
+//  square.style.backgroundColor = "red"
+window.addEventListener("scroll", function(){
+    //instrukacje warunkowe na rosniecie i zmniejszanie 
+    if(grow == true){
+        size += 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+    else{
+        size -= 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+    if(size >= window.innerWidth / 2){
+        grow = false
+    }
+    else if ( size <= 0){
+        grow = true;
+    }
+})
